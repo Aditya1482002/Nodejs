ad8081080f41bfcab9cb59e5f020a8a2cb620745
Aditya1482002 <aditya1482002@gmail.com>
2022-11-25 18:34:14 +0530

    committed

--- NodeJS-main/nodeJS/nodeJS/nodeAssignment/aread.js
+++ NodeJS-main/nodeJS/nodeJS/nodeAssignment/aread.js
@@ -1,6 +1,6 @@
-const fs = require('fs');//in built module
+const fs = require('fs');//in built module  './demofile.txt'
 
-fs.readFile('./demofile.txt', 'utf8', (err, data) => {
+fs.readFile('C:\Users\User\Desktop\mevnAssignment\nodejs\NodeJS-main\nodeJS\nodeJS\Sample\demofile.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
