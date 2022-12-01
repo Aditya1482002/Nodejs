4bacdf9618925331511fbc5cc3479975abbd6542
Aditya1482002 <aditya1482002@gmail.com>
2022-12-01 16:11:18 +0530

    commited...

--- NodeJS-main/nodeJS/nodeJS/nodeAssignment/acreatedb/app.js
+++ NodeJS-main/nodeJS/nodeJS/nodeAssignment/acreatedb/app.js
@@ -1,3 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0/college",{ useNewUrlParser : true,})
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0/college",
+{ useNewUrlParser : true, })
 .then(()=> console.log("connection successful")).catch((err)=>console.log(err));
+
