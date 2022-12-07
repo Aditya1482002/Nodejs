621a528d36233b93a9614852c8ae8e366a10715e
Aditya1482002 <aditya1482002@gmail.com>
2022-12-07 11:25:52 +0530

    commited

---
+++ NodeJS-main/nodeJS/nodeJS/nodeAssignment/all_asssignment/find.js
@@ -0,0 +1,11 @@
+const { MongoClient, url, client} = require('./connection.js');
+//const url='mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0';
+MongoClient.connect(url,function(err,db){
+    if(err) throw err;
+    var dbo=db.db('college');
+    dbo.collection('student').findOne({},function(err,result){
+        if(err) throw err;
+        console.log(result.name);
+        db.close();
+    });
+});
